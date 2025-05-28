719256d80b3b4cbc3514ab07a55885a50e0028fe
victor <vialepa@proton.me>
2025-05-27 19:17:03 -0600

    Continuacion de curso en casa, next thema Class

--- Basic/09_loops.py
+++ Basic/09_loops.py
@@ -37,16 +37,16 @@ print("La ejecución continúa")
 
 # For
 """nos sirve para iterar un listado de elementos"""
-print("inicia fode list")
+print("inicia for list que guardan elementos una a uno")
 my_list = [35, 24, 62, 52, 30, 30, 17]
 
 for element in my_list:
     print(element)
-"""mientras que while hace que el codigo se repita varias veces en funcion de una condicion, 
-for hace que se repita tantas veces como elementos que tengamos iterables y cada vez que le de 
-una vuelta a for va a estar accediendo a uno de esos elementos del listado """
+"""mientras que while hace que el codigo se repita infinitas veces en funcion de una condicion, 
+for hace que se repita tantas veces como elementos que tengamos iterables por lo que es finita 
+y cada vez que le de una vuelta a for va a estar accediendo a uno de esos elementos del listado"""
 
-print("inicia for tuple")
+print("inicia for tuple guardan elementos pero no se pueden modificar")
 my_tuple = (35, 1.77, "Brais", "Moure", "Brais")
 
 for element in my_tuple:
@@ -54,13 +54,16 @@ for element in my_tuple:
 
 my_set = {"Brais", "Moure", 35}
 
-print("inicia for set")
+print("inicia for set, guardan elementos que no esten repetidos")
 for element in my_set:
     print(element)
 
 my_dict = {"Nombre": "Brais", "Apellido": "Moure", "Edad": 35, 1: "Python"}
 
-print("inicia for dict")
+print("inicia for dict, guardan elementos clave -> valor pero con for solo muestra el keys "
+"y si queremos ver los vlaores entonces convertiriamos el dict a list y quedaria como"
+"for element in list(my_dict.values()): ó solo my_dict.values(): porque en si values es "
+"capaz de traerlos en formato lista ")
 for element in my_dict:
     print(element)
     if element == "Edad":
@@ -73,7 +76,9 @@ print("La ejecución continúa")
 for element in my_dict:
     print(element)
     if element == "Edad":
-        continue
-    print("Se ejecuta")
+        continue #es lo contrario a brake y muestra 1 en este caso, no tiene tanto sentido 
+    print("Se ejecuta continue")
 else:
     print("El bluce for para diccionario ha finalizado")
+"""continue es es vuelva a for sin ver lo que esta debajo
+break es termina todo el bucle"""
--- Basic/10_functions.py
+++ Basic/10_functions.py
@@ -3,25 +3,44 @@
 ### Functions ###
 
 # Definición
+"""resolver una logica concreta que intruduzcamos y 
+ nos hace evitar errores porque la solucion a ese problema siempre en ese codigo
+ y de alguna manera estara centralizada esa solucion y ya solo la tendremos que 
+ mandar llamar, def es la palabra reservada para functions
+ def nombre_function(): y todo lo tab despues de esto sera la funcion"""
 
 def my_function():
     print("Esto es una función")
 
 
+my_function() 
 my_function()
 my_function()
-my_function()
+"""si ejecutamos no estariamos mirando el print dentro de def sino que se queda esperando
+a ser llamada y ahora con my_fuction() ya se ejecuta eso que es la funcion en nuestro caso
+es print("Esto es una funcion"), por lo que ya no necesitamos mas codigo para agregar de nuevo
+ese print solo necesitamos llamar de nuevo la def
 
-# Función con parámetros de entrada/argumentos
+Con esto nos ahorramos codigo para cosas mas complejas y nos previene errores al intentar copiar
+todo el codigo si no tuvieramos def"""
+ 
+
+# Función con parámetros de entrada/argumentos 
+"no se limita solo a desencadenar codigo,"
+" tambien puede devolver y recibir codigo"
 
 
-def sum_two_values(first_value: int, second_value):
+def sum_two_values(first_value, second_value): 
     print(first_value + second_value)
+"""esta funcion es distitna a la anterior porque aqui el fist y secund value que estan
+dentro de los () nos pide que le demos dos valores de entrada, por eso es un parametro 
+de entrada y ademas estamos diciendo que se imprima la suma de ambos valores
 
+Por lo que aqui lineas despues estamos dando esa entrada de paramentros a la def"""
 
 sum_two_values(5, 7)
 sum_two_values(54754, 71231)
-sum_two_values("5", "7")
+sum_two_values("5", "7") #57 porque concatana las dos cadenas de texto, xloq tipa es dinamico
 sum_two_values(1.4, 5.2)
 
 # Función con parámetros de entrada/argumentos y retorno
@@ -32,11 +51,12 @@ def sum_two_values_with_return(first_value, second_value):
     return my_sum
 
 
-my_result = sum_two_values(1.4, 5.2)
+my_result = sum_two_values(1.4, 5.2) #retorna none porque sum_two_values es una def sin retorno
 print(my_result)
 
-my_result = sum_two_values_with_return(10, 5)
-print(my_result)
+my_result = sum_two_values_with_return(10, 5) #resulado de la def la asignas a una variable
+print(my_result) #y esta si retorna algo porque sum_two_value_with_return es una def de retorno
+
 
 # Función con parámetros de entrada/argumentos por clave
 
@@ -48,17 +68,17 @@ def print_name(name, surname):
 print_name(surname="Moure", name="Brais")
 
 # Función con parámetros de entrada/argumentos por defecto
-
+"""dentro de def podemos dalrle un valor por defecto para que si no se agrega se muestre ese
+valor, en este caso alias ="sin alias" pero como si tenemos alias que es MoureDev
+nos da el parametro correcto"""
 
 def print_name_with_default(name, surname, alias="Sin alias"):
     print(f"{name} {surname} {alias}")
+print_name_with_default("Brais", "Moure") #sin valor de alias
+print_name_with_default("Brais", "Moure", "MoureDev") #con valor de alias "MoureDev" 
 
-
-print_name_with_default("Brais", "Moure")
-print_name_with_default("Brais", "Moure", "MoureDev")
-
-# Función con parámetros de entrada/argumentos arbitrarios
-
+# Función con parámetros de entrada/argumentos arbitrarios y dinamicos
+"""con * en los paramentros le pedimos que le podemos pasar cualquiera"""
 
 def print_upper_texts(*texts):
     print(type(texts))
